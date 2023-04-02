812a49deb927aad544fbc71fe4e249eab25406d3
ahmed samir <ahmed.samir.farid@gmail.com>
2023-04-02 10:35:19 +0200

    V 0.1

---
+++ components/Footer/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TheFooter"
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout">
+    <the-header />
+    <Nuxt  />
+
+    <the-footer />
+  </div>
+</template>
+
+<script>
+import TheFooter from '~/components/Footer/TheFooter.vue'
+import TheHeader from '~/components/Header/TheHeader.vue'
+export default {
+  components: { TheHeader, TheFooter },
+
+}
+</script>
+
+<style>
+
+</style>
